76737ad46b78e10d95141204bcdfc5e68eecb996
rniochy <lniochy@gmail.com>
2022-12-05 09:23:28 +0100

    created main method to organize app struture into js code

--- js/app.js
+++ js/app.js
@@ -1,13 +1,26 @@
 const startButton = document.getElementById("mailbox-button-start");
-const stoptButton = document.getElementById("mailbox-button-stop");
+const stopButton = document.getElementById("mailbox-button-stop");
 const resetButton = document.getElementById("mailbox-button-reset");
+const numberOfInterval = document.getElementById("mailbox-monitoring-interval");
 const mailbox = document.getElementById("mailbox-data-content");
 
-const iotmailbox = new IOTMailbox(500, ()=>0);
-iotmailbox.startMonitoring();
 
 
-startButton.addEventListener("click", ()=>{
-    window.setTimeout(()=> iotmailbox.stopMonitoring(), 5000);
+function main(){
 
-})
+    const iotmailbox = new IOTMailbox(500, ()=>0);
+    
+    
+    
+    startButton.addEventListener("click", ()=>{
+        iotmailbox.startMonitoring();
+        window.setTimeout(()=> iotmailbox.stopMonitoring(), 5000);
+    })
+    
+    stopButton.addEventListener("click", ()=>{
+       mailbox.stopMonitoring();
+    
+    })
+}
+
+main();
